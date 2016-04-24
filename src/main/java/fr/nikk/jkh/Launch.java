22b23bdedae04568f892273d687a21a12b50ca21
alexgus <alex.guyon78@gmail.com>
2016-04-24 16:57:43 +0200

    Launch class created

---
+++ src/main/java/fr/nikk/jkh/Launch.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.nikk.jkh;
+
+/**
+ * @author Alexandre Guyon
+ *
+ */
+public class Launch {
+
+	/**
+	 * Main of the application
+	 * @param args No args
+	 */
+	public static void main(String[] args) {
+		System.out.println("It works !");
+	}
+
+}
